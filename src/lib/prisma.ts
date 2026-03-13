3f19fc77b07a1d766e965491b00285639a246490
viktor <vikbobinski@gmail.com>
2026-03-13 20:36:26 +0100

    x

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,16 @@
 import { PrismaClient } from "@/generated/prisma/client";
+import path from "path";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: `file:${path.join(process.cwd(), "app_data", "database", "dev.db")}`,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
